eff7aea3ad9f7784c18d51836ea407420662b4ef
Oleg V <ioleffka@gmail.com>
2020-09-18 11:04:54 +0000

    consistency between playbooks (regex updated), try/except added for testing purposes into filter_plugin

--- filter_plugins/get_playbook_name.py
+++ filter_plugins/get_playbook_name.py
@@ -1,15 +1,19 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import sys
 import re
 
 
 def run(message):
     pattern_filter = '[a-zA-Z0-9_-]*[.]ya?ml'
-    find = re.search(pattern_filter, message)
+    cmdline = " ".join(sys.argv)
 
-    return find.group()
-
+    try:
+      find = re.search(pattern_filter, cmdline)
+      return find.group()
+    except:
+      AttributeError
 
 class FilterModule(object):
     def filters(self):
